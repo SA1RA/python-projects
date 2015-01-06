9e8a875803ec0590f7d5d8c67ad3b3b229ea3827
syanjiave <smlucas13@gamil.com>
2015-01-06 11:07:38 -0500

    python project self destruct in 5

---
+++ only 4 seconds.py
@@ -0,0 +1,11 @@
+import time
+
+countdown = 5
+
+while countdown > 0:
+    print('self destructing in ...')
+    print(countdown)
+    countdown -= 1
+    time.sleep(1)
+
+print('B' + 'O' * 100 + 'M!!')
